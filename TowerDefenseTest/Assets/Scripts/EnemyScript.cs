a1e2dc6040eece88ecdc28fa57f38565e9db3577
Vítor Neto <vitordiogo2000@sapo.pt>
2023-03-24 00:43:44 +0000

    Added base game world level 1 and enemy AI base

---
+++ TowerDefenseTest/Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour
+{
+
+    public float speed = 10f;
+    private Transform target;
+    private int wavepointIndex = 0;
+
+    void Start()
+    {
+        target = WaypointsScript.waypoints[0];    
+    }
+
+
+    void Update()
+    {
+        Vector3 dir = target.position - transform.position;
+        transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World);
+
+        if(Vector3.Distance(transform.position, target.position) <= 0.4f)
+        {
+            GetNextWaypoint();
+        }
+    }
+
+    void GetNextWaypoint()
+    {
+        if(wavepointIndex >= WaypointsScript.waypoints.Length - 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            wavepointIndex++;
+            target = WaypointsScript.waypoints[wavepointIndex];
+        }
+        
+    }
+}
